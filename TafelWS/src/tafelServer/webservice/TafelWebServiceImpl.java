b62c20d886992123a882aafac93dff75e90c1f05
Simon Bastian <s9sibast@stud.uni-saarland.de>
2017-01-23 13:49:25 +0100

    anfang webservice implementierung

--- TafelWS/src/tafelServer/webservice/TafelWebServiceImpl.java
+++ TafelWS/src/tafelServer/webservice/TafelWebServiceImpl.java
@@ -1,16 +1,23 @@
 package tafelServer.webservice;
 
+import java.net.InetSocketAddress;
 import java.util.Date;
+import java.util.LinkedList;
 
 import javax.jws.WebService;
 
 import tafelServer.TafelServer;
+import verteilteAnzeigetafel.Message;
 import verteilteAnzeigetafel.TafelException;
 
 @WebService(endpointInterface = "tafelServer.webservice.TafelWebService")
 public class TafelWebServiceImpl implements TafelWebService {
-
 	TafelServer tafelServer;
+
+//	public TafelWebServiceImpl(int abteilungsID) {
+//		super();
+//		this.tafelServer = new TafelServer(abteilungsID);
+//	}
 
 	public String createMessage(String inhalt, int user, int abtNr, boolean oeffentlich) {
 		if (tafelServer != null) {
@@ -27,20 +34,43 @@ public class TafelWebServiceImpl implements TafelWebService {
 
 	public String deleteMessage(int messageID, int user) {
 		if (tafelServer != null) {
-			// delete message
+			String answer = "";
+			try {
+				answer = tafelServer.deleteMessage(messageID, user);
+			} catch (TafelException e) {
+				answer = e.getMessage();
+			}
+			return answer;
 		}
 		return null;
 	}
 
 	public String modifyMessage(int messageID, String inhalt, int user) {
 		if (tafelServer != null) {
-			// modify message
+			String answer = "";
+			try {
+				answer = tafelServer.modifyMessage(messageID, inhalt, user);
+			} catch (TafelException e) {
+				answer = e.getMessage();
+			}
+			return answer;
 		}
 		return null;
 	}
 
 	public String publishMessage(int messageID, int user) {
-
+		if (tafelServer != null) {
+			String answer = "";
+			try {
+				answer = tafelServer.publishMessage(messageID, user);
+			} catch (InterruptedException e) {
+				tafelServer.printStackTrace(e);
+				answer = "Internal Server Error: e.getMessage()";
+			} catch (TafelException e) {
+				answer = e.getMessage();
+			}
+			return answer;
+		}
 		return null;
 	}
 
@@ -63,21 +93,56 @@ public class TafelWebServiceImpl implements TafelWebService {
 		return null;
 	}
 
+	// TODO format?
 	@Override
 	public String[] showMessages(int user) {
-		// TODO Auto-generated method stub
-		return null;
+		int size = 0;
+		boolean success = false;
+		String errorMessage = "";
+		LinkedList<Message> messagesByUserID = new LinkedList<Message>();
+		try {
+			messagesByUserID = tafelServer.getMessagesByUserID(user);
+			size = messagesByUserID.size() + 1;
+			success = true;
+		} catch (TafelException e) {
+			size = 2;
+			errorMessage = e.getMessage();
+		}
+		String[] strings = new String[size];
+		strings[0] = Boolean.toString(success);
+		if (!success) {
+			strings[1] = errorMessage;
+		} else {
+			for (int i = 1; i < strings.length; i++) {
+				strings[i] = messagesByUserID.get(i - 1).toString();
+			}
+		}
+
+		return strings;
 	}
 
 	@Override
-	public boolean receiveMessage(int messageID, int userID, int abtNr, String inhalt, boolean oeffentlich,
-			Date time) {
-		return false;
+	public boolean receiveMessage(int messageID, int userID, int abtNr, String inhalt, boolean oeffentlich, Date time) {
+		try {
+			tafelServer.receiveMessage(messageID, userID, abtNr, inhalt, oeffentlich, time);
+		} catch (TafelException e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean registerServer(int abtNr, String address) {
-		// TODO Auto-generated method stub
+		String[] addressParts = address.split(":");
+		try {
+			tafelServer.registerTafel(abtNr, new InetSocketAddress(addressParts[0], Integer.parseInt(addressParts[1])));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TafelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
