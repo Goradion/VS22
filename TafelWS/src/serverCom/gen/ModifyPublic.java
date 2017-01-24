19b5c0a10fe20dd535ae30c650ddac962f803da8
Dan340 <hui>
2017-01-24 15:24:28 +0100

    ServerCom WS wsimport generated

---
+++ TafelWS/src/serverCom/gen/ModifyPublic.java
@@ -0,0 +1,117 @@
+
+package serverCom.gen;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für modifyPublic complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="modifyPublic">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="arg1" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="arg2" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="arg3" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "modifyPublic", propOrder = {
+    "arg0",
+    "arg1",
+    "arg2",
+    "arg3"
+})
+public class ModifyPublic {
+
+    protected int arg0;
+    protected int arg1;
+    protected int arg2;
+    protected String arg3;
+
+    /**
+     * Ruft den Wert der arg0-Eigenschaft ab.
+     * 
+     */
+    public int getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Legt den Wert der arg0-Eigenschaft fest.
+     * 
+     */
+    public void setArg0(int value) {
+        this.arg0 = value;
+    }
+
+    /**
+     * Ruft den Wert der arg1-Eigenschaft ab.
+     * 
+     */
+    public int getArg1() {
+        return arg1;
+    }
+
+    /**
+     * Legt den Wert der arg1-Eigenschaft fest.
+     * 
+     */
+    public void setArg1(int value) {
+        this.arg1 = value;
+    }
+
+    /**
+     * Ruft den Wert der arg2-Eigenschaft ab.
+     * 
+     */
+    public int getArg2() {
+        return arg2;
+    }
+
+    /**
+     * Legt den Wert der arg2-Eigenschaft fest.
+     * 
+     */
+    public void setArg2(int value) {
+        this.arg2 = value;
+    }
+
+    /**
+     * Ruft den Wert der arg3-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getArg3() {
+        return arg3;
+    }
+
+    /**
+     * Legt den Wert der arg3-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setArg3(String value) {
+        this.arg3 = value;
+    }
+
+}
