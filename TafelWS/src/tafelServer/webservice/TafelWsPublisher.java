ae80a0587a39a4c6699259ba109685018f3f2b49
ki-am <alexander_mueller1987@web.de>
2017-01-27 14:46:09 +0100

    TafelWsPublisher message dialog mit button ersetzt

--- TafelWS/src/tafelServer/webservice/TafelWsPublisher.java
+++ TafelWS/src/tafelServer/webservice/TafelWsPublisher.java
@@ -1,33 +1,59 @@
 package tafelServer.webservice;
 
-import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.xml.ws.Endpoint;
 
 import tafelServer.TafelServer;
 
 public class TafelWsPublisher {
-
+	public static final String STOPWS = "Stop web service";
+	
+	private Endpoint clientEndpoint = Endpoint.create(new TafelWebServiceImpl());
+	private Endpoint serverEndpoint = Endpoint.create(new ServerComWebserviceImpl());
+	
+	private String address = "localhost";
+	private int abteilung = 1;
 	public static void main(String[] args) {
-		TafelServer tafelServer;
-		int abteilung = 1;
-		String address = "localhost";
+		final TafelWsPublisher publisher = new TafelWsPublisher();
 		if (args.length >= 1) {
 			try {
-				abteilung = Integer.parseInt(args[0]);
+				publisher.abteilung = Integer.parseInt(args[0]);
 			} catch (NumberFormatException nfe) {
 				// default to abteilung 1;
 			}
 		}
 		if (args.length >= 2) {
-			address = args[1];
+			publisher.address = args[1];
 		}
 
-		TafelServer.startServer(abteilung);
-		Endpoint clientEndpoint = Endpoint.publish("http://" + address + ":8080/TafelWS/tafelws", new TafelWebServiceImpl());
-		Endpoint serverEndpoint = Endpoint.publish("http://" + address + ":8080/TafelWS/serverws", new ServerComWebserviceImpl());
-		JOptionPane.showMessageDialog(null, "Server beenden");
-		clientEndpoint.stop();
-		serverEndpoint.stop();
+		TafelServer.startServer(publisher.abteilung);
+		
+		publisher.clientEndpoint.publish("http://" + publisher.address + ":8080/TafelWS/tafelws");
+		publisher.serverEndpoint.publish("http://" + publisher.address + ":8080/TafelWS/serverws");
+		
+		final JFrame jFrame = new JFrame("TafelWSPublisher: Tafel " + publisher.abteilung +" WS - published");
+		jFrame.setSize(400, 200);
+		jFrame.setResizable(false);
+
+		final JButton jButton = new JButton(STOPWS);
+		jButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				publisher.clientEndpoint.stop();
+				publisher.serverEndpoint.stop();
+				jFrame.setTitle("TafelWSPublisher: Tafel " + publisher.abteilung +" WS - stopped");
+				jButton.setText("WS is stopped!");
+			}
+
+		});
+
+		jFrame.add(jButton);
+		jFrame.setVisible(true);
 
 	}
 
