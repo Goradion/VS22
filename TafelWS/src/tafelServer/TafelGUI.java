1c48838b0a0e56d20e9dfc7eadb6c43c48542ae1
Simon Bastian <s9sibast@stud.uni-saarland.de>
2017-01-27 17:23:09 +0100

    umstellung auf Map/Set

--- TafelWS/src/tafelServer/TafelGUI.java
+++ TafelWS/src/tafelServer/TafelGUI.java
@@ -2,106 +2,97 @@ package tafelServer;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
+
 import verteilteAnzeigetafel.Anzeigetafel;
 import verteilteAnzeigetafel.Message;
 
 public class TafelGUI implements Observer {
-    
-    private final JFrame window;
-    private JTextArea localMessages;
-    private JTextArea globalMessages;
-    private JScrollPane localScroll;
-    private JScrollPane globalScroll;
-    private JTextArea[] gruppenMessages;
-    public JFrame getWindow() {
+
+	private final JFrame window;
+	private JTextArea localMessages;
+	private JTextArea globalMessages;
+	private JScrollPane localScroll;
+	private JScrollPane globalScroll;
+	private Map<Integer, JTextArea> gruppenMessages;
+
+	public JFrame getWindow() {
 		return window;
 	}
 
-	private JScrollPane[] gruppenScroll;
-    private int[] gruppen;
-    
-    TafelGUI(int abteilung, TafelServer ts,int[] gruppen){
-        this.window = new JFrame("Abteilung "+Integer.toString(abteilung));
-        this.gruppen = gruppen;
-        gruppenMessages = new JTextArea[gruppen.length];
-        gruppenScroll = new JScrollPane[gruppen.length];
-        for(int i = 0; i< gruppen.length;i++){
-        	gruppenMessages[i] = new JTextArea();
-        	gruppenMessages[i].setEditable(false);
-        	gruppenScroll[i] = new JScrollPane (); 
-        	
-        	           
-        	
-        	
-        }
+	private Map<Integer, JScrollPane> gruppenScroll;
+	private Set<Integer> gruppen;
 
-        
-        for(int i = 0; i< gruppen.length;i++){
-        	this.gruppenScroll[i] = new JScrollPane(gruppenMessages[i]);
-            gruppenScroll[i].setBorder(new TitledBorder("Gruppe " + gruppen[i] ));
-        }
+	TafelGUI(int abteilung, TafelServer ts, Set<Integer> set) {
+		this.window = new JFrame("Abteilung " + Integer.toString(abteilung));
+		this.gruppen = set;
+		gruppenMessages = new HashMap<Integer, JTextArea>();
+		gruppenScroll = new HashMap<Integer, JScrollPane>();
+		for (Integer i : set) {
+			JTextArea jTextArea = new JTextArea();
+			JScrollPane jScrollPane = new JScrollPane(jTextArea);
+			jScrollPane.setBorder(new TitledBorder("Gruppe " + i));
+			jTextArea.setEditable(false);
+			gruppenMessages.put(i, jTextArea);
+			gruppenScroll.put(i, jScrollPane);
 
-        this.localMessages = new JTextArea();
-        localMessages.setEditable(false);
+		}
 
-        
-        this.globalMessages = new JTextArea();
-        globalMessages.setEditable(false);
+		this.localMessages = new JTextArea();
+		localMessages.setEditable(false);
 
-        
-        
-        this.localScroll = new JScrollPane(localMessages);
-        localScroll.setBorder(new TitledBorder("Local messages"));
+		this.globalMessages = new JTextArea();
+		globalMessages.setEditable(false);
 
-        
-        this.globalScroll = new JScrollPane(globalMessages);
-        globalScroll.setBorder(new TitledBorder("Global messages"));
+		this.localScroll = new JScrollPane(localMessages);
+		localScroll.setBorder(new TitledBorder("Local messages"));
 
-        
-        window.setLayout(new GridLayout(gruppen.length + 2,1));
-        
-        
-        window.add(localScroll);
-        for(int i = 0; i < gruppen.length;i++)
-        {
-        	window.add(gruppenScroll[i]);
-        }
-        window.add(globalScroll);
-        
-        window.setPreferredSize(new Dimension(400,gruppen.length *200 +300));
-        window.setSize(new Dimension(400,gruppen.length *200 +300));
-        window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        
-        window.setVisible(true);
-       
-    }
-    
-    @Override
-    public void update(Observable o, Object arg) {
-        Anzeigetafel at = (Anzeigetafel)o;
-        localMessages.setText(null);
-        globalMessages.setText(null);
-        for(Message m: at.getLocalMsgs()){
-            localMessages.append("UserID: " + m.getUserID()+'\t'+"Zeit: "
-                    +m.getTime()+'\n'+m.getInhalt()+'\n'+'\n');
-        }
-        for(Message m: at.getGlobalMsgs()){
-            globalMessages.append("Abt: "+m.getAbtNr()+'\t'+"UserID: "
-                    + m.getUserID()+'\t'+"Zeit: "+m.getTime()+'\n'
-                    +m.getInhalt()+'\n'+'\n');
-        }
-        for(int i = 0; i < gruppen.length;i++){
-	        for(Message m: at.getGroupMsgs(gruppen[i])){
-	            gruppenMessages[i].append("Abt: "+m.getAbtNr()+'\t'+"UserID: "
-	                    + m.getUserID()+'\t'+"Zeit: "+m.getTime()+'\n'
-	                    +m.getInhalt()+'\n'+'\n');
-	        }
-        }
-    }    
+		this.globalScroll = new JScrollPane(globalMessages);
+		globalScroll.setBorder(new TitledBorder("Global messages"));
+
+		window.setLayout(new GridLayout(set.size() + 2, 1));
+
+		window.add(localScroll);
+		for (JScrollPane jScrollPane : gruppenScroll.values()) {
+			window.add(jScrollPane);
+		}
+		window.add(globalScroll);
+
+		window.setPreferredSize(new Dimension(400, set.size() * 200 + 300));
+		window.setSize(new Dimension(400, set.size() * 200 + 300));
+		window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		window.setVisible(true);
+
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		Anzeigetafel at = (Anzeigetafel) o;
+		localMessages.setText(null);
+		globalMessages.setText(null);
+		for (Message m : at.getLocalMsgs()) {
+			localMessages.append(
+					"UserID: " + m.getUserID() + '\t' + "Zeit: " + m.getTime() + '\n' + m.getInhalt() + '\n' + '\n');
+		}
+		for (Message m : at.getGlobalMsgs()) {
+			globalMessages.append("Abt: " + m.getAbtNr() + '\t' + "UserID: " + m.getUserID() + '\t' + "Zeit: "
+					+ m.getTime() + '\n' + m.getInhalt() + '\n' + '\n');
+		}
+		for (Integer i : gruppen) {
+			for (Message m : at.getGroupMsgs(i)) {
+				gruppenMessages.get(i).append("Abt: " + m.getAbtNr() + '\t' + "UserID: " + m.getUserID() + '\t'
+						+ "Zeit: " + m.getTime() + '\n' + m.getInhalt() + '\n' + '\n');
+			}
+		}
+	}
 }
