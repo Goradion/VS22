25ff48cc58b219abd4b13bb5960771e5cc9c63cf
Simon Bastian <s9sibast@stud.uni-saarland.de>
2017-01-30 14:08:27 +0100

    tafelAdressen

--- TafelWS/src/client/Client.java
+++ TafelWS/src/client/Client.java
@@ -2,6 +2,10 @@ package client;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.ReadOnlyFileSystemException;
@@ -10,6 +14,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -20,6 +25,7 @@ import client.gen.SoapableMessage;
 import client.gen.TafelWebService;
 import client.gen.TafelWebServiceImplService;
 import verteilteAnzeigetafel.Message;
+import verteilteAnzeigetafel.TafelException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -40,15 +46,19 @@ public class Client {
 	private static Map<Integer, Message> msgs;
 	private static TafelWebService port;
 	private static URL wsdlURL;
+	private static HashMap<Integer, URL> tafelAdressen = new HashMap<Integer, URL>();
+	private static final int koordinatorID = 1;
 
 	public static void main(String[] args) throws MalformedURLException {
-		if (args.length >= 1) {
-			wsdlURL = new URL(args[0]);
-		} else {
-			wsdlURL = new URL("http://localhost:8080/TafelWS/tafelws?wsdl");
-		}
-		
-		Integer[] abteilungen = new Integer[] { 1, 2, 3 };
+//		if (args.length >= 1) {
+//			wsdlURL = new URL(args[0]);
+//		} else {
+//			wsdlURL = new URL("http://10.9.41.69:8080/TafelWS/tafelws?wsdl");
+//		}
+		loadTafelAdressenFromFile();
+		Set<Integer> keySet = tafelAdressen.keySet();
+		Integer[] abteilungen = new Integer[keySet.size()];
+		keySet.toArray(abteilungen);
 		msgs = new HashMap<Integer, Message>();
 		
 		clientGui = new ClientGui("Tafel-Client", abteilungen);
@@ -68,7 +78,7 @@ public class Client {
 			clientGui.showLoggedIn(clientGui.getUserid(), msgs);
 			clientGui.setMenue(menue);
 
-			clientGui.setPublish(clientGui.getUserid() == 1);
+			clientGui.setPublish(clientGui.getUserid() == koordinatorID);
 			clientGui.actionMenuSelect(new java.awt.event.ItemListener() {
 				public void itemStateChanged(java.awt.event.ItemEvent evt) {
 					menueSelected(evt);
@@ -80,7 +90,8 @@ public class Client {
 				}
 			});
 			clientGui.setConectivity(false);
-			port = new TafelWebServiceImplService(wsdlURL).getTafelWebServiceImplPort();
+			System.out.println(tafelAdressen.get(clientGui.getAbteilung()));
+			port = new TafelWebServiceImplService(tafelAdressen.get(clientGui.getAbteilung())).getTafelWebServiceImplPort();
 			clientGui.setConectivity(true);
 			
 			resetMenu();
@@ -95,7 +106,7 @@ public class Client {
 		case "Neue Nachricht":
 			clientGui.setMenue(new String[] { "Neue Nachricht", "Zeige alle Nachrichten" });
 			clientGui.showNewMessage();
-			clientGui.setPublish(clientGui.getUserid() == 1);
+			clientGui.setPublish(clientGui.getUserid() == koordinatorID);
 			clientGui.actionSendNewMessage(new java.awt.event.ActionListener() {
 				public void actionPerformed(ActionEvent evt) {
 					sendNewMessage(evt);
@@ -209,7 +220,7 @@ public class Client {
 		int userid = clientGui.getUserid();
 		Message selectedMessage = clientGui.getSelectedMessage();
 		if (selectedMessage == null) {
-			JOptionPane.showMessageDialog(clientGui, "Es gibt keine nachricht mit der gew�hlten ID!");
+			JOptionPane.showMessageDialog(clientGui, "Es gibt keine nachricht mit der gewälten ID!");
 			return;
 		}
 		String oeffentlichString = selectedMessage.isOeffentlich() ? "oeffentliche" : "lokale";
@@ -227,11 +238,11 @@ public class Client {
 	private static void editMessage(ActionEvent evt) {
 		Message selectedMessage = clientGui.getSelectedMessage();
 		if (selectedMessage == null) {
-			JOptionPane.showMessageDialog(clientGui, "Es gibt keine nachricht mit der gew�hlten ID!");
+			JOptionPane.showMessageDialog(clientGui, "Es gibt keine nachricht mit der gewählten ID!");
 			return;
 		}
 		clientGui.showEditMessage(selectedMessage);
-		clientGui.setPublish(clientGui.getUserid() == 1);
+		clientGui.setPublish(clientGui.getUserid() == koordinatorID);
 		clientGui.addActionChangeButton((new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				submitChangedMessage(evt);
@@ -278,6 +289,8 @@ public class Client {
 		List<SoapableMessage> showMessages = port.showMessages(clientGui.getUserid());
 		msgs.clear();
 		for (SoapableMessage soapableMessage : showMessages) {
+			if (soapableMessage.isOeffentlich() ){
+			}
 			Message message = new Message(soapableMessage.getMessageID(), soapableMessage.getUserID(),
 					soapableMessage.getAbtNr(), soapableMessage.getInhalt(), soapableMessage.isOeffentlich(),
 					soapableMessage.getTime().toGregorianCalendar().getTime());
@@ -292,11 +305,37 @@ public class Client {
 	private static void resetMenu() {
 		clientGui.setMenue(new String[] { "Zeige alle Nachrichten", "Neue Nachricht" });
 		updateMessages();
-		clientGui.setPublish(clientGui.getUserid() == 1);
+		clientGui.setPublish(clientGui.getUserid() == koordinatorID);
 		clientGui.addActionSendQuery(new java.awt.event.ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				sendQueryActionPerformed(evt);
 			}
 		});
+	}
+	
+	private static void loadTafelAdressenFromFile() {
+		int lines = 0;
+		try (BufferedReader reader = new BufferedReader(new FileReader("./tafelAdressen"))) {
+			String address = "";
+			while ((address = reader.readLine()) != null) {
+				lines++;
+				String[] addressParts = address.split(" ");
+				try {
+					tafelAdressen.put(Integer.parseInt(addressParts[0]), new URL(addressParts[1]));
+				} catch (NumberFormatException e) {
+					System.err.println("NumberFormatException in line: " + lines);
+					e.printStackTrace();
+				} catch (MalformedURLException e){
+					System.err.println("MalformedURLException in line: " + lines);
+				}
+			}
+			System.out.println("Tafel Adressen geladen.");
+		} catch (FileNotFoundException e) {
+			System.err.println("Keine tafelAdressen Datei gefunden.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Fehler beim Lesen der tafelAdressen Datei.");
+			e.printStackTrace();
+		}
 	}
 }
