29b92f23ac3bb914437807b8e33ed6e3d7255891
Dan340 <hui>
2017-03-18 03:54:21 +0100

    Kommunikation Funktioniert!     C++ Client hat mittels der receiveMessage eine Nachricht an den Java Server geschickt und wurde auf der Anzeige-Tafel angezeigt. Ein Java Client konnte diese auch wie gehabt auslesen.
    Um es kompilieren zu können muss folgende lib (im 2. Post) libws2_32.a beim g++ command angegeben werden:
    http://stackoverflow.com/questions/2795760/using-gsoap-in-qt-windows
    g++ -L"YOUR\MINGW\PATH\TO\LIB\FOLDER\WHERE\libws2_32\IS" -lws2_32    ....
    
    Ich nutze Eclipse_C++, dort in den Projekt Properties unter C/C++ Build => Settings => Tool Settings im Linker => Libraries
    oben bei Libraries (-l) ws2_32 eintragen darunter bei Library search path (-L) den Pfad zum Ordner

--- Client_C++/src/RunClient.cpp
+++ Client_C++/src/RunClient.cpp
@@ -22,15 +22,15 @@ int main ()
     {
         ServerComWebserviceImplPortBindingProxy service;
         ns1__receiveMessage * arguments = new ns1__receiveMessage();
-        arguments->arg0 = 1; // messageID
+        arguments->arg0 = 2; // messageID
         arguments->arg1 = 1; // userID
-        arguments->arg2 = 1; // abtNr
+        arguments->arg2 = 2; // abtNr
         arguments->arg3 = "Hallo test";  // inhalt
         arguments->arg4 =  "Fri Mar 17 16:26:13 CET 2017";   // time TODO passendes Datenformat für Time
         arguments->arg5 = 1; // group
         ns1__receiveMessageResponse results;
         if ( service.receiveMessage(arguments, results) == SOAP_OK )
-            cout << "The receiveMessage went: " << results.return_ << endl;  // get the methods (receiveMessage) return value
+            cout << "The receiveMessage: " << results.return_ << endl;  // get the methods (receiveMessage) return value
         else
             service.soap_stream_fault(cerr);
         service.destroy(); // delete data and release memory
