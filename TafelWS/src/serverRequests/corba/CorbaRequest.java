7142983faa54ddbae2a754f40e621811d1d8f891
Simon Bastian <s9sibast@stud.uni-saarland.de>
2017-03-27 15:35:21 +0200

    serializable

--- TafelWS/src/serverRequests/corba/CorbaRequest.java
+++ TafelWS/src/serverRequests/corba/CorbaRequest.java
@@ -1,8 +1,13 @@
 package serverRequests.corba;
 
+import java.io.Serializable;
+
 import tafelServer.CorbaRequestDeliver;
 
-public abstract class CorbaRequest {
+public abstract class CorbaRequest implements Serializable {
+	
+	protected static final long serialVersionUID = 164552615903886460L;
+		
 	private String messageID;
 	private int userID;
 	
